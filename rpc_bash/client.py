87f34b2c8c4bef87e019d74278da875bdd1d58dd
Jakub Nurski <nurskijakub@gmail.com>
2018-10-07 20:36:56 +0200

    Implemented stdout and stderr.

--- rpc_bash/client.py
+++ rpc_bash/client.py
@@ -1,21 +1,30 @@
 import asyncio
+import sys
 
 from rpcudp.protocol import RPCProtocol
 
 
-@asyncio.coroutine
-def sayhi(rpc_protocol, address):
-    result = yield from rpc_protocol.sayhi(address, "Snake Plissken")
-    print(result[1] if result[0] else "No response received.")
+class RPCClient(RPCProtocol):
+
+    async def listen(self):
+        while True:
+            command = input()
+            await self.command(('127.0.0.1', 1234), command)
+
+    async def rpc_stdout(self, sender, message):
+        sys.stdout.write(message.decode())
+        return True
+
+    async def rpc_stderr(self, sender, message):
+        sys.stderr.write(message.decode())
+        return True
 
 
 loop = asyncio.get_event_loop()
-listen = loop.create_datagram_endpoint(RPCProtocol, local_addr=('127.0.0.1', 4567))
-transport, protocol = loop.run_until_complete(listen)
+listen = loop.create_datagram_endpoint(RPCClient, local_addr=('127.0.0.1', 4567))
+_, client = loop.run_until_complete(listen)
 
-func = sayhi(protocol, ('127.0.0.1', 1234))
 try:
-    loop.run_until_complete(func)
-    loop.run_forever()
+    loop.run_until_complete(client.listen())
 except KeyboardInterrupt:
-    pass
+    loop.close()
--- rpc_bash/server.py
+++ rpc_bash/server.py
@@ -1,16 +1,44 @@
 import asyncio
+from subprocess import Popen, PIPE
+from threading import Thread
 
 from rpcudp.protocol import RPCProtocol
 
 
 class RPCServer(RPCProtocol):
-    # Any methods starting with "rpc_" are available to clients.
-    def rpc_sayhi(self, sender, name):
-        # This could return a Deferred as well. sender is (ip,port)
-        return "Hello %s you live at %s:%i" % (name, sender[0], sender[1])
+
+    def __init__(self):
+        super().__init__()
+        self.pipe = None
+
+    async def rpc_command(self, sender, command):
+        pipe = Popen(command, stdout=PIPE, stdin=PIPE, stderr=PIPE, shell=True)
+        stdout_thread = Thread(target=self.send_stdout, args=(sender, pipe))
+        stdout_thread.run()
+        stderr_thread = Thread(target=self.send_stderr, args=(sender, pipe))
+        stderr_thread.run()
+        return True
+
+    def send_stdout(self, sender, pipe):
+        for line in iter(pipe.stdout.readline, 'b'):
+            if line:
+                self.stdout(sender, line)
+            else:
+                break
+
+    def send_stderr(self, sender, pipe):
+        for line in iter(pipe.stderr.readline, 'b'):
+            if line:
+                self.stderr(sender, line)
+            else:
+                break
 
 
 loop = asyncio.get_event_loop()
 listen = loop.create_datagram_endpoint(RPCServer, local_addr=('127.0.0.1', 1234))
 transport, protocol = loop.run_until_complete(listen)
-loop.run_forever()
+
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    loop.close()
