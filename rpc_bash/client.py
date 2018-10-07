e777998afcdcac876549dd64f987b10d7a30774c
Jakub Nurski <nurskijakub@gmail.com>
2018-10-07 21:21:22 +0200

    Added argparse to server and client.

--- rpc_bash/client.py
+++ rpc_bash/client.py
@@ -1,3 +1,4 @@
+import argparse
 import asyncio
 import sys
 
@@ -6,10 +7,15 @@ from rpcudp.protocol import RPCProtocol
 
 class RPCClient(RPCProtocol):
 
+    def __init__(self, server_ip, server_port):
+        super().__init__()
+        self.server_ip = server_ip
+        self.server_port = server_port
+
     async def listen(self):
         while True:
             command = input()
-            await self.command(('127.0.0.1', 1234), command)
+            await self.command((self.server_ip, self.server_port), command)
 
     async def rpc_stdout(self, sender, message):
         sys.stdout.write(message.decode())
@@ -20,11 +26,29 @@ class RPCClient(RPCProtocol):
         return True
 
 
-loop = asyncio.get_event_loop()
-listen = loop.create_datagram_endpoint(RPCClient, local_addr=('127.0.0.1', 4567))
-_, client = loop.run_until_complete(listen)
+def run_client(client_ip, client_port, server_ip, server_port):
+    loop = asyncio.get_event_loop()
 
-try:
-    loop.run_until_complete(client.listen())
-except KeyboardInterrupt:
-    loop.close()
+    def spawn_client():
+        return RPCClient(server_ip, server_port)
+
+    listen = loop.create_datagram_endpoint(spawn_client, local_addr=(client_ip, client_port))
+    _, client = loop.run_until_complete(listen)
+
+    try:
+        loop.run_until_complete(client.listen())
+    except KeyboardInterrupt:
+        loop.close()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--address', type=str, required=True, help="Address to bind")
+    parser.add_argument('--server_address', type=str, required=True, help="Server address")
+
+    args = parser.parse_args(sys.argv[1:])
+
+    _client_ip, _client_port = args.address.split(':')
+    _server_ip, _server_port = args.server_address.split(':')
+
+    run_client(_client_ip, int(_client_port), _server_ip, int(_server_port))
--- rpc_bash/server.py
+++ rpc_bash/server.py
@@ -1,4 +1,6 @@
+import argparse
 import asyncio
+import sys
 from subprocess import Popen, PIPE
 from threading import Thread
 
@@ -34,11 +36,23 @@ class RPCServer(RPCProtocol):
                 break
 
 
-loop = asyncio.get_event_loop()
-listen = loop.create_datagram_endpoint(RPCServer, local_addr=('127.0.0.1', 1234))
-transport, protocol = loop.run_until_complete(listen)
+def run_server(server_ip, server_port):
+    loop = asyncio.get_event_loop()
+    listen = loop.create_datagram_endpoint(RPCServer, local_addr=(server_ip, server_port))
+    loop.run_until_complete(listen)
 
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    loop.close()
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        loop.close()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--address', type=str, required=True, help="Address to bind")
+
+    args = parser.parse_args(sys.argv[1:])
+
+    ip, port = args.address.split(':')
+
+    run_server(ip, int(port))
